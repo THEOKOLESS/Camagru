7779bb679602074642dbca49a1576da3847810fe
Theophile Vitoux <tvitoux@f0r5s13.codam.nl>
2020-11-29 21:18:14 +0100

    clean git

--- view/Validation/invalidation.php
+++
@@ -1,7 +0,0 @@
-<?php $title = 'Erreur validation compte'; ?>
-<?php ob_start(); ?>
-
-    <p>Erreur ! Votre compte ne peut être activé...</p>
-    
-<?php $content = ob_get_clean(); ?>
-<?php require('view/template.php'); ?>
